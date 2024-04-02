157095fbf71527ce20a53c023dc4b185cce84c19
Kristofer Putsinin <kristofer.putsinin@transferwise.com>
2024-04-02 19:49:32 +0300

    add menu tabs

--- src/routes/forecast/container/FinancialForecastContainer.tsx
+++ src/routes/forecast/container/FinancialForecastContainer.tsx
@@ -1,21 +1,37 @@
-import { Col, Row, Tabs, TabsProps } from "antd";
-import ProductOverview from "../../product/ProductOverview";
+import {Col, Row, Tabs, TabsProps, Tag} from "antd";
+import ProductContainer from "../../product/ProductContainer";
 import { FinancialForecastService } from "../../../services/FinancialForecastService";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import {
+  DiffOutlined,
+  BarChartOutlined,
   PieChartOutlined,
   FallOutlined,
   RiseOutlined,
 } from "@ant-design/icons";
 import ExpensesContainer from "../../expenses/ExpensesContainer";
+import IncomeView from "../../income/IncomeView";
+import {Product} from "../../../domain/Product";
 
 export const getPercents = (num: number) => {
   return `${num * 100} %`;
 };
 
+export const roundNumberToTwoDecimalPlaces = (num: number) => Math.round((num + Number.EPSILON) * 100) / 100
+
 export const getPrice = (price: number) => {
   return `${price} €`;
+};
+
+export const getLatestYear = (products: Product[]) => {
+  return products.reduce((prev, next) => {
+    const maxYearFromProduct = next.productsPerPeriod.reduce(
+        (a, b) => Math.max(a, b.year),
+        new Date().getFullYear(),
+    );
+    return Math.max(prev, maxYearFromProduct);
+  }, new Date().getFullYear());
 };
 
 const FinancialForecastContainer = () => {
@@ -38,7 +54,7 @@ const FinancialForecastContainer = () => {
         </span>
       ),
       children: (
-        <ProductOverview
+        <ProductContainer
           fetchForecast={getForecastById.refetch}
           financialForecast={getForecastById.data}
         />
@@ -51,7 +67,7 @@ const FinancialForecastContainer = () => {
           <RiseOutlined /> Raha sissetulek
         </span>
       ),
-      children: <></>,
+      children: <IncomeView/>,
     },
     {
       key: "3",
@@ -67,13 +83,35 @@ const FinancialForecastContainer = () => {
         />
       ),
     },
+    {
+      key: "4",
+      label: (
+        <span>
+          <BarChartOutlined /> Kasumiaruanne
+        </span>
+      ),
+      children: (
+        <>VARSTI TULEKUL</>
+      ),
+    },
+    {
+      key: "5",
+      label: (
+        <span>
+          <DiffOutlined /> Bilanss
+        </span>
+      ),
+      children: (
+        <>VARSTI TULEKUL</>
+      ),
+    },
   ];
 
   return (
     <>
       <Row justify="center">
         <Col>
-          <h1> Finantsprognoos: {getForecastById.data?.name} </h1>
+          <h1><Tag color="blue"> <h1>Finantsprognoos: {getForecastById.data?.name} </h1></Tag></h1>
         </Col>
       </Row>
       <div style={{ marginLeft: "1rem" }}>
