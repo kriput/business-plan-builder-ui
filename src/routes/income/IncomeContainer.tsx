f42237b13a4fd64863e7d219e5211b180e4102f4
Kristofer Putsinin <kristofer.putsinin@transferwise.com>
2024-04-05 18:54:56 +0300

    income container update

--- src/routes/income/IncomeContainer.tsx
+++ src/routes/income/IncomeContainer.tsx
@@ -1,34 +1,51 @@
-import { Col, Divider, Row, Skeleton } from "antd";
-import {
-  countTotalForAllOperationsPerPeriod,
-  getTotalsPerPeriod,
-} from "../forecast/container/FinancialForecastContainer";
-import { ProductPerPeriod } from "../../domain/ProductPerPeriod";
+import { Col, Divider, Row, Skeleton, Tag } from "antd";
 import { useQuery } from "@tanstack/react-query";
 import { FinancialOperationService } from "../../services/FinancialOperationService";
 import { useState } from "react";
 import { FinancialOperation } from "../../domain/FinancialOperation";
 import SimpleTotalPerPeriodTable from "../../base_components/financial_operation/SimpleTotalPerPeriodTable";
-import FinancialOperationOverview from "../../base_components/financial_operation/FinancialOperationOverview";
+import FinancialOperationOverview, {
+  parseToFinancialOperationSubtype,
+} from "../../base_components/financial_operation/FinancialOperationOverview";
 import { FinancialOperationType } from "../../enums/FinancialOperationType";
 import ErrorResult from "../../base_components/ErrorResult";
 import { INCOME_CATEGORY_LIST } from "./IncomeCategoryListCreator";
+import { FinancialOperationSubtype } from "../../enums/FinancialOperationSubtype";
+import { VAT } from "../../index";
 
 interface Props {
   forecastId: number;
   latestYear: number;
 }
 
-interface IncomeData {
-  period: number;
-  totalIncome: number;
-  incomeWithTax: number;
-  incomeFromExport: number;
+const subTypesNotIncludedForTotal = [
+  FinancialOperationSubtype.SALES_INCOME_WITH_TAX,
+  FinancialOperationSubtype.SALES_INCOME_WITHOUT_TAX,
+];
+
+const countVATForPeriod = (incomes: FinancialOperation[], year: number) => {
+  const filteredIncomes = incomes.filter(income =>
+      parseToFinancialOperationSubtype(income.subtype!) !== FinancialOperationSubtype.SALES_INCOME &&
+      parseToFinancialOperationSubtype(income.subtype!) !== FinancialOperationSubtype.SALES_INCOME_WITHOUT_TAX);
+  let sum = 0;
+  for (const income of filteredIncomes) {
+    if (!income.tax) {
+      sum += income.totalsPerPeriod.filter(tPP => tPP.year === year).flat().reduce((a, b) => a + b.sum * VAT, 0)
+    } else {
+      sum += income.totalsPerPeriod.filter(tPP => tPP.year === year).flat().reduce((a, b) => a + b.sum * (income.tax ?? VAT) , 0)
+    }
+  }
+  return sum;
 }
 
-const getTotalSoldForExportRate = (productsPerPeriod: ProductPerPeriod[]) =>
-  productsPerPeriod.map((p) => p.forExport).reduce((a, b) => a + b, 0) /
-  productsPerPeriod.length;
+const countTotalForAllOperationsPerPeriod = (incomes: FinancialOperation[], year: number) => {
+  const total = incomes.filter((income) => !subTypesNotIncludedForTotal.includes(parseToFinancialOperationSubtype(income.subtype!)))
+  .map(income => income.totalsPerPeriod).flat()
+  .filter(tPP => tPP.year === year)
+  .reduce((a, b) => a + b.sum, 0);
+  return total + countVATForPeriod(incomes, year);
+}
+
 
 const IncomeContainer = (props: Props) => {
   const financialOperationService = new FinancialOperationService();
@@ -55,31 +72,46 @@ const IncomeContainer = (props: Props) => {
       <Row justify="center">
         <Col>
           <SimpleTotalPerPeriodTable
-              dataProcessor={countTotalForAllOperationsPerPeriod}
-              totalsPerPeriod={getTotalsPerPeriod(incomes)}
-              latestYear={props.latestYear}
-              addFirstBlank={false}
+            dataProcessor={countTotalForAllOperationsPerPeriod}
+            financialOperations={incomes}
+            latestYear={props.latestYear}
+            addFirstBlank={false}
           />
         </Col>
       </Row>
       <Divider />
 
       {getIncomes.isSuccess && (
+        <>
           <FinancialOperationOverview
-              financialOperationType={FinancialOperationType.INCOME}
-              forecastId={props.forecastId}
-              latestYear={props.latestYear}
-              financialOperations={incomes}
-              financialOperationCategoryList={INCOME_CATEGORY_LIST}
-              title="Laekumised müügist"
+            financialOperationType={FinancialOperationType.INCOME}
+            forecastId={props.forecastId}
+            latestYear={props.latestYear}
+            financialOperations={incomes}
+            financialOperationCategoryList={INCOME_CATEGORY_LIST}
+            title="Laekumised müügist"
           />
+          <Row>
+            <Col span={20}>
+              <Tag style={{ width: "100%" }} color="red">
+                <h3>Operatsioonidelt makstav käibemaks: </h3>
+                <SimpleTotalPerPeriodTable
+                  addFirstBlank={true}
+                  latestYear={props.latestYear}
+                  financialOperations={incomes}
+                  dataProcessor={countVATForPeriod}
+                />
+              </Tag>
+            </Col>
+          </Row>
+        </>
       )}
       {getIncomes.isError && (
-          <ErrorResult
-              errorMessage={getIncomes.error.message}
-              buttonMessage={"Proovi uuesti"}
-              onClick={() => getIncomes.refetch()}
-          />
+        <ErrorResult
+          errorMessage={getIncomes.error.message}
+          buttonMessage={"Proovi uuesti"}
+          onClick={() => getIncomes.refetch()}
+        />
       )}
       {getIncomes.isPending && <Skeleton active />}
     </>
